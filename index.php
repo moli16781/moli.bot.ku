910b60f05e7e29d309864dc79903c3ad198876b8
moli <moli16781@gmail.com>
2021-02-07 00:15:11 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+http_response_code(200);
+die;
+// header("Content-Type: application/json");
+switch ($_GET['v']) {
+    case '1':
+        require 'v1/controller.php';
+        break;
+    default:
+        echo json_encode(array('status' => false, 'msg' => 'VERSION: Not correct.'));
+        exit(0);
+        break;
+}
+?>
