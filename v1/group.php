8a3702861810ee3789fabe545d8d2a23adf17d47
moli <moli16781@gmail.com>
2021-02-07 17:17:17 +0700

    Update group.php

--- v1/group.php
+++ v1/group.php
@@ -1,5 +1,12 @@
 <?php
 $messages = array();
+$textReturn = "รายละเอียด \n" . $content . " \n";
+array_push($messages, array(
+    'type' => 'text',
+    'text' => $textReturn
+));
+
+
 $getUserGroup = $line->getProfileGroup($groupId,$userID,$strAccessToken);
 $textReturn = "รายละเอียด \n" . $getUserGroup . " \n";
 array_push($messages, array(
