fb3c3410c26b59f256a633985e6f1a9f194a254c
moli <moli16781@gmail.com>
2021-02-07 15:58:11 +0700

    Update group.php

--- v1/group.php
+++ v1/group.php
@@ -1,14 +1,23 @@
 <?php
-
-// $getUser = json_decode($line->getProfileGroup($groupId,$userID,$strAccessToken));       
-$textReturn .= "pictureUrl " . $groupId . " \n";
-$textReturn .= "displayName " . $userID . " \n";
-$textReturn .= "All " . $strAccessToken . " \n";
+$getUser = json_decode($line->getProfileGroup($groupId,$userID,$strAccessToken));
 
 $messages = array();
+
+$textReturn = "pictureUrl " . $groupId . " \n";
 array_push($messages, array(
     'type' => 'text',
     'text' => $textReturn
 ));
+$textReturn = "displayName " . $userID . " \n";
+array_push($messages, array(
+    'type' => 'text',
+    'text' => $textReturn
+));
+$textReturn = "All " . $strAccessToken . " \n";
+array_push($messages, array(
+    'type' => 'text',
+    'text' => $textReturn
+));
+
 
 reply($replyToken,'',$messages);
