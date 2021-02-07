affe2bcebc585bf6166b3e73bc60d36893953ac0
moli <moli16781@gmail.com>
2021-02-07 16:19:20 +0700

    Update group.php

--- v1/group.php
+++ v1/group.php
@@ -2,14 +2,9 @@
 $getUserGroup = json_decode($line->getProfileGroup($groupId,$userID,$strAccessToken));
 
 $messages = array();
-
 array_push($messages, array(
     'type' => 'text',
-    'text' => $getUserGroup->displayName
-));
-array_push($messages, array(
-    'type' => 'text',
-    'text' => $getUserGroup->pictureUrl
+    'text' => $getUserGroup
 ));
 
 
