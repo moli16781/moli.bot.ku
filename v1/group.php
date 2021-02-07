bbbff92ff83ff98ca67df0e0417e770a6724d973
moli <moli16781@gmail.com>
2021-02-07 16:55:21 +0700

    Update group.php

--- v1/group.php
+++ v1/group.php
@@ -1,11 +1,17 @@
 <?php
-// $getUserGroup = json_decode($line->getProfileGroup($groupId,$userID,$strAccessToken));
-
 $messages = array();
-$textReturn = "รายละเอียด \n" . $content . " \n";
+$getUserGroup = $line->getProfileGroup($groupId,$userID,$strAccessToken);
+$textReturn = "รายละเอียด \n" . $getUserGroup . " \n";
 array_push($messages, array(
     'type' => 'text',
     'text' => $textReturn
 ));
+$getUserGroup = json_decode($getUserGroup);
+$textReturn = "รายละเอียด \n" . $getUserGroup . " \n";
+array_push($messages, array(
+    'type' => 'text',
+    'text' => $textReturn
+));
+
 
 reply($replyToken,'',$messages);
