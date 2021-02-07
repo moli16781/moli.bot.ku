28ea3cf46a5ddaf9d5c1fff8be229f3eddd8269f
moli <moli16781@gmail.com>
2021-02-07 17:00:05 +0700

    Update group.php

--- v1/group.php
+++ v1/group.php
@@ -7,7 +7,8 @@ array_push($messages, array(
     'text' => $textReturn
 ));
 $getUserGroup = json_decode($getUserGroup);
-$textReturn = "รายละเอียด \n" . $getUserGroup->userID . " \n";
+$textReturn = "displayName \n" . $getUserGroup->displayName . " \n";
+$textReturn .= "pictureUrl \n" . $getUserGroup->pictureUrl . " \n";
 array_push($messages, array(
     'type' => 'text',
     'text' => $textReturn
